05b5b6f214bc955add7d6aaf1b8b0b090e4d9e57
ViniciusBelt <super.vi@hotmail.com>
2022-08-05 12:03:38 -0300

    Aula atual: 45

---
+++ desafio3/app.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0
+    },
+    computed: {
+        resultado(){
+            return this.valor == 37 ?
+                'Valor Igual' : 'Valor Diferente'
+        },
+    },
+    watch: {
+        valor(){
+            setTimeout(() => {
+                this.valor = 0;
+            }, 5000);
+        }
+    },
+    methods: {
+        
+    },
+});
