7140429c9d6617bfcd88bfc49d764dba67bd8604
ViniciusBelt <super.vi@hotmail.com>
2022-08-17 14:26:05 -0300

    Aula Atual: 239

---
+++ http/http-exercicios/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+	<div id="app" class="container">
+		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome:">
+				<b-form-input type="text" size="lg"
+					v-model="usuario.nome"
+					placeholder="Informe o Nome"></b-form-input>
+			</b-form-group>
+		</b-card>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	},
+	// created() {
+	// 	this.$http.post('usuarios.json', {
+	// 		nome: 'Maria',
+	// 		email: 'maria@gmail.com'
+	// 	}).then(res => console.log(res))
+	// }
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	font-size: 1.5rem;
+}
+
+#app h1 {
+	text-align: center;
+	margin: 50px;
+}
+</style>
