9155a4ca421ae37b824b6fc0f4c5428049942c3f
ViniciusBelt <super.vi@hotmail.com>
2022-08-23 09:34:04 -0300

    Aula Atual: 292

--- Vuex/vuex-exercicios/src/components/Carrinho.vue
+++ Vuex/vuex-exercicios/src/components/Carrinho.vue
@@ -26,22 +26,20 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
     computed: {
-        total() {
-            return this.produtos.map(p => p.quantidade * p.preco)
-                .reduce((total, atual) => total + atual, 0)
+        ...mapGetters({
+            total: 'valorTotal'
+        }),
+        // total() {
+        //     return this.$store.getters.valorTotal
+        // },
+        produtos() {
+            return this.$store.state.produtos
         }
     },
-    data() {
-        return {
-            produtos: [
-                { id: 1, nome: 'Produto 1', quantidade: 7, preco: 14.55 },
-                { id: 2, nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-                { id: 3, nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-            ]
-        }
-    }
 }
 </script>
 
--- Vuex/vuex-exercicios/src/components/Loja.vue
+++ Vuex/vuex-exercicios/src/components/Loja.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 
 export default {
     data() {
@@ -21,6 +22,7 @@ export default {
         }
     },
     methods: {
+        ...mapMutations(['adicionarProduto']),
         adicionar() {
             const produto = {
                 id: this.sequencia,
@@ -29,8 +31,10 @@ export default {
                 preco: this.preco
             }
             this.sequencia++
-            // eslint-disable-next-line
-            console.log(produto)
+            
+            // this.$store.state.produtos.push(produto)
+            // this.$store.commit('adicionarProduto', produto)
+            this.adicionarProduto(produto)
         }
     }
 }
